03a4868bc68495b196b57ed9ff0b8f3cfbb3d436
Adrian Matei <adrian.matei@bkw.ch>
2015-07-03 17:14:55 +0200

    further refactoring

---
+++ api/src/main/java/org/podcastpedia/api/manifest/ImplementationDetails.java
@@ -0,0 +1,45 @@
+package org.podcastpedia.api.manifest;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
+@XmlAccessorType(XmlAccessType.FIELD)
+public class ImplementationDetails {
+
+	@XmlElement(name = "implementationTitle")	
+	String implementationTitle;
+
+	@XmlElement(name = "implementationVersion")	
+	String implementationVersion;
+	
+	@XmlElement(name = "implementationVendorId")	
+	String implementationVendorId;
+
+	public String getImplementationTitle() {
+		return implementationTitle;
+	}
+
+	public void setImplementationTitle(String implementationTitle) {
+		this.implementationTitle = implementationTitle;
+	}
+
+	public String getImplementationVersion() {
+		return implementationVersion;
+	}
+
+	public void setImplementationVersion(String implementationVersion) {
+		this.implementationVersion = implementationVersion;
+	}
+
+	public String getImplementationVendorId() {
+		return implementationVendorId;
+	}
+
+	public void setImplementationVendorId(String implementationVendorId) {
+		this.implementationVendorId = implementationVendorId;
+	}
+		
+}
