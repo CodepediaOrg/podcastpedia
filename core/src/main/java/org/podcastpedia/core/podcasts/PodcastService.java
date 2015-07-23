da8d9bc3136b2a4ef9b76bedd0f183e15d991f56
amacoder <ama@codingpedia.org>
2015-07-23 08:28:36 +0200

    refactoring - renamed method

--- core/src/main/java/org/podcastpedia/core/podcasts/PodcastService.java
+++ core/src/main/java/org/podcastpedia/core/podcasts/PodcastService.java
@@ -8,39 +8,39 @@ import java.util.List;
 
 
 /**
- * Service interface, mainly used to access the DAO layer. 
- * 
+ * Service interface, mainly used to access the DAO layer.
+ *
  * @author amasia
  *
  */
 @Service("podcastService")
 public interface PodcastService {
-		
+
 	/**
 	 * Returns a podcast by its id
-	 * 
+	 *
 	 * @param podcastId
 	 * @return
 	 */
 	public Podcast getPodcastById(int podcastId)  throws BusinessException;
 
 	/**
-	* Retunrs the number of episodes available for the given podcast 
+	* Retunrs the number of episodes available for the given podcast
 	* @param podcastId
 	* @return
 	*/
 	public Integer getNumberEpisodesForPodcast(Integer podcastId);
 
 	/**
-	* Return attributes of the podcast given the feed url 
-	* 
-	* 
-	* @param numberOfPodcasts (number of podcasts to be returned)
+	* Return attributes of the podcast given the feed url
+	*
+	*
+	* @param feedUrl (number of podcasts to be returned)
 	* @return
 	*/
 	public List<Podcast> getPodcastAttributesByFeedUrl(String feedUrl);
 
-	public Podcast getPodcastForIdentifier(String identifier)  throws BusinessException;		  
-	  
+	public Podcast getPodcastByIdentifier(String identifier)  throws BusinessException;
+
 }
 
