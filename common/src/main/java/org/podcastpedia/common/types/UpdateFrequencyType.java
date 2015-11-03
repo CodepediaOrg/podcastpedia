8a4aee43a28a1f30973ed137b64924e371964d7b
amacoder <ama@codingpedia.org>
2015-11-03 06:31:57 +0100

    put weekly frequency first

--- common/src/main/java/org/podcastpedia/common/types/UpdateFrequencyType.java
+++ common/src/main/java/org/podcastpedia/common/types/UpdateFrequencyType.java
@@ -10,45 +10,45 @@ import java.util.Map;
  *
  */
 public enum UpdateFrequencyType {
-	
-	/** podcast is updated daily*/
-	DAILY(1),
-	
+
 	/** podcast is updated weekly */
 	WEEKLY(2),
-	
+
+    /** podcast is updated daily*/
+    DAILY(1),
+
 	/** podcast is updated monthly */
 	MONTHLY(3),
-	
+
 	/** podcast is updated yearyl */
 	YEARLY(4),
-	
+
 	/** podcast is most likely through, and it will never be updated - but contents are valuable and stored somewhere */
 	TERMINATED(5),
-	
+
 	/** the update frequency cannot be deducted or is unknown*/
 	UNKNOWN(6);
-	
+
 	private int code;
-	
+
 	private UpdateFrequencyType(int code) {
 		this.code = code;
 	}
-	
+
 	public int getCode() {
 		return code;
 	}
-	
-	private static final Map<Integer,UpdateFrequencyType> lookup 
+
+	private static final Map<Integer,UpdateFrequencyType> lookup
     = new HashMap<Integer,UpdateFrequencyType>();
 
 	static {
 	    for(UpdateFrequencyType s : EnumSet.allOf(UpdateFrequencyType.class))
 	         lookup.put(s.getCode(), s);
-	}	
-	
-    public static UpdateFrequencyType get(int code) { 
-        return lookup.get(code); 
+	}
+
+    public static UpdateFrequencyType get(int code) {
+        return lookup.get(code);
    }
-    
+
 }
