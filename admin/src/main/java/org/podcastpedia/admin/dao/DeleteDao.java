bfc9cbd34ee7df613e95a2b7cfb78fc1dff6d73d
amacoder <ama@codingpedia.org>
2016-03-29 06:54:15 +0200

    add the possibility for the admin to remove users (from users, subscriptions and authorities) from the admin console

--- admin/src/main/java/org/podcastpedia/admin/dao/DeleteDao.java
+++ admin/src/main/java/org/podcastpedia/admin/dao/DeleteDao.java
@@ -8,64 +8,70 @@ public interface DeleteDao {
 	 * deletes from Database the podcast with the given podcast id
 	 */
 	public void deletePodcastById(int podcastId);
-	
+
 	/**
 	 * deletes all episodes from the database with the given podcast id (basically the episodes that belong to
 	 * the given podcast by its id)
-	 * 
+	 *
 	 * @param podcastId
 	 */
 	public void deleteEpisodesByPodcastId(int podcastId);
-	
+
 	/**
 	 * deletes the podcast_categories entries from the database with the given podcast id (basically the episodes that belong to
 	 * the given podcast by its id)
-	 * 
+	 *
 	 * @param podcastId
 	 */
-	public void deletePodcastCategoriesByPodcastId(int podcastId);	
-	
+	public void deletePodcastCategoriesByPodcastId(int podcastId);
+
 	/**
-	 * deletes the podcast from the database with the given feedUrl (this is as UNIQUE in the db persisted) 
+	 * deletes the podcast from the database with the given feedUrl (this is as UNIQUE in the db persisted)
 	 * @param feedUrl
 	 */
 	public void deletePodcastByFeedUrl(String feedUrl);
-	
+
 	/**
-	 * deletes the episodes from db for the podcast with the given feedUrl 
-	 *  
+	 * deletes the episodes from db for the podcast with the given feedUrl
+	 *
 	 * @param feedUrl
 	 */
-	public void deleteEpisodesByFeedUrl(String feedUrl);	
-	
+	public void deleteEpisodesByFeedUrl(String feedUrl);
+
 	/**
-	 * deletes the podcast_categories entries from db for the podcast with the given feedUrl 
-	 *  
+	 * deletes the podcast_categories entries from db for the podcast with the given feedUrl
+	 *
 	 * @param feedUrl
 	 */
-	public void deletePodcastCategoriesByFeedUrl(String feedUrl);	
-	
+	public void deletePodcastCategoriesByFeedUrl(String feedUrl);
+
 	/**
-	 * deletes all episodes from the database 
+	 * deletes all episodes from the database
 	 */
 	public void deleteAllEpisodes();
 
 	/**
-	 * delete PodcastTags given the podcast id 
+	 * delete PodcastTags given the podcast id
 	 * @param podcastId
 	 */
 	public void deletePodcastTagsByPodcastId(Integer podcastId);
-	
+
 	/**
-	 * delete PodcastTags given the podcast's feed url 
+	 * delete PodcastTags given the podcast's feed url
 	 * @param feedUrl
 	 */
-	public void deletePodcastTagsByFeedUrl(String feedUrl);	
-	
+	public void deletePodcastTagsByFeedUrl(String feedUrl);
+
 	/**
-	 * deletes the list of episodes from the database 
-	 * 
+	 * deletes the list of episodes from the database
+	 *
 	 * @param episodes
 	 */
 	public void deleteEpisodes(EpisodeListToBeDeleted episodes);
+
+    void deleteUserSubscriptions(String email);
+
+    void deleteUser(String email);
+
+    void deleteUserFromAuthorities(String email);
 }
