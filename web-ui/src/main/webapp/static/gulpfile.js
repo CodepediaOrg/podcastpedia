958d0ed17cfc3935710faf6fd77511d4650824e6
Adrian Matei <adrianmatei@gmail.com>
2016-04-21 17:22:32 +0200

    reorganize gulp tasks default gulp taks does now a build of the application

--- web-ui/src/main/webapp/static/gulpfile.js
+++ web-ui/src/main/webapp/static/gulpfile.js
@@ -5,16 +5,14 @@ var gulp = require("gulp"),//http://gulpjs.com/
 		autoprefixer = require('gulp-autoprefixer'),//https://www.npmjs.org/package/gulp-autoprefixer
 		minifycss = require('gulp-minify-css'),//https://www.npmjs.org/package/gulp-minify-css
 		rename = require('gulp-rename'),//https://www.npmjs.org/package/gulp-rename,
-    //clean  = require('gulp-clean'),//https://www.npmjs.com/package/gulp-clean --deprecated
     del=require('del'),//https://github.com/gulpjs/gulp/blob/master/docs/recipes/delete-files-folder.md
-    uglify=require('gulp-uglify'),//https://www.npmjs.com/package/gulp-uglify,
+    uglify=require('gulp-uglify'),//https://www.npmjs.com/package/gulp-uglify
     concat=require('gulp-concat'),//https://github.com/wearefractal/gulp-concat
     jshint=require('gulp-jshint'),//https://www.npmjs.com/package/gulp-jshint
     wrapper = require('gulp-wrapper'), //https://www.npmjs.com/package/gulp-wrapper
 		log = util.log;
 
 var sassFiles = "src/sass/**/*.scss";
-
 gulp.task("sass", function(){
   log("Generate CSS files " + (new Date()).toString());
   gulp.src(sassFiles)
@@ -25,19 +23,21 @@ gulp.task("sass", function(){
       .pipe(minifycss())
       .pipe(gulp.dest('target/css'));
 
-  gulp.src('src/resources/fonts/**/*.{ttf,woff,eof,svg,eot}')
-    .pipe(gulp.dest('target/css/fonts'));
 });
 
 gulp.task("watch", function(){
   log("Watching scss files for modifications");
   gulp.watch(sassFiles, ["sass"]);
+
+  log("Watching js files for modifications");
+  gulp.watch(jsFiles, ["compress:js"]);
+
 });
 
 //copy fonts from source ("static/src/fonts")to the "css" directory because they are referenced by the css files
 gulp.task('copy:fonts', function() {
-    gulp.src('src/resources/fonts/**/*.{ttf,woff,eof,svg,eot}')
-        .pipe(gulp.dest('css/fonts'));
+  gulp.src('src/resources/fonts/**/*.{ttf,woff,eof,svg,eot}')
+    .pipe(gulp.dest('target/css/fonts'));
 });
 
 //deletes content of the target directory, sort of a "maven clean" functionality
@@ -47,11 +47,12 @@ gulp.task('clean', function(cb){
 
 //delete content of "css" folder, before creating a new one
 gulp.task('clean:css', function(cb){
-  del.sync('css/**/*', cb);//https://github.com/sindresorhus/del#delsyncpatterns-options
+  del.sync('target/css/**/*', cb);//https://github.com/sindresorhus/del#delsyncpatterns-options
 });
 
+var jsFiles = "src/js/pages/**/*.js";
 gulp.task('compress:js', function() {
-    return gulp.src('src/js/pages/**/*.js')
+    return gulp.src(jsFiles)
         .pipe(concat('app.js'))
         //.pipe(uglify())
         .pipe(wrapper({
@@ -68,5 +69,8 @@ gulp.task('jshint', function() {
         .pipe(jshint.reporter('jshint-stylish'));
 });
 
-//when running just 'gulp' in the command line only the "sass" task will get executed
-gulp.task("default", ["clean", "sass", 'compress:js']);
+
+gulp.task("build", ["clean", "copy:fonts", "sass", 'compress:js']);
+
+//task is executed when running only the "gulp" command
+gulp.task("default", ["build"]);
